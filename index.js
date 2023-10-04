55fe92f39355116ae2b6efef3624c0cc69c78349
alejogar2014 <alejogar1986@gmail.com>
2023-10-03 20:11:46 -0500

    Auth

--- index.js
+++ index.js
@@ -1,28 +1,61 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+const log = require("./logs/logs");
 
-require('dotenv').config()
-const port = process.env.PORT || 5000
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
-// !important! 
+// !important!
 // you need to install the following libraries |express|[dotenv > if required]
-// or run this command >> npm i express dotenv 
+// or run this command >> npm i express dotenv
 
-app.get('/', (req, res) => {
-
-    res.send('SecurOS Enterprise - D-Fense Integration V1.0')
-
-})
-app.post('/dfense', (req, res) => {
-
-    res.send('hello from simple server :)')
-
-})
+app.get("/", (req, res) => {
+  res.send("SecurOS Enterprise - D-Fense Integration V1.0");
+});
 
 
+function isAuth(req, res, next) {
+  const auth = req.headers.authorization;
+  if (auth === 'Basic aXNzOmlzcw==') {
+    next();
+  } else {
+    res.status(401);
+    res.send('Access forbidden');
+  }
+}
 
+app.post("/dfense",isAuth, (req, res) => {
 
+  if (!isObjectEmpty(req.body)) {
+    console.log(req);
+    let response = {};
+    response.data = req.body;
+    response.status = "Received";
+    response.date = time();
+    log.Write(JSON.stringify(response), "INFO", "dfense.index");
+    res.send(response);
+    return;
+  }
 
-app.listen(port, () => console.log('> Server is up and running on port : ' + port))
+  res.send("empty");
+});
+
+const isObjectEmpty = (objectName) => {
+  return (
+    objectName &&
+    Object.keys(objectName).length === 0 &&
+    objectName.constructor === Object
+  );
+};
+
+app.listen(port, () =>
+  console.log("> Server is up and running on port : " + port)
+);
+
+const time = () => {
+  let ts = Date.now();
+  let date_ob = new Date(ts);
+  return date_ob.toLocaleString()
+};
---
+++ logs/config.js
@@ -0,0 +1,3 @@
+exports.logs_amount = '3';
+exports.log_size = '50000';
+exports.level = 'DEBUG';
